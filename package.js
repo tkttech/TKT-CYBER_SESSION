7dcdcad01a0c6ea1b855a08d9dd5ccf50b94d12a
terrizevdev <terrizevdev@gmail.com>
2025-07-12 21:51:54 +0300

    Update package.js

--- package.js
+++ package.js
@@ -4,13 +4,11 @@
   "description": "WhatsApp pairing system for ORMAN-XMD bot",
   "main": "index.js",
   "engines": {
-    "npm": ">=9.7.2",
     "node": ">=20.0.0"
   },
   "scripts": {
     "start": "node index.js",
-    "dev": "nodemon index.js",
-    "test": "echo \"Error: no tests specified\" && exit 1"
+    "dev": "nodemon index.js"
   },
   "dependencies": {
     "@whiskeysockets/baileys": "latest",
@@ -19,15 +17,11 @@
     "pino": "^8.1.0",
     "fs-extra": "^11.1.1",
     "axios": "^1.6.2",
-    "qrcode": "^1.5.3",
-    "awesome-phonenumber": "^2.64.0",
-    "phone": "3.1.30",
     "path": "^0.12.7",
     "cors": "^2.8.5",
-    "helmet": "^7.1.0",
-    "morgan": "^1.10.0"
+    "helmet": "^7.1.0"
   },
   "devDependencies": {
     "nodemon": "^3.0.2"
   }
-  }
+}
