be0edcc7265befac4e8b2df0d3d0b3ebe644e55f
terrizevdev <terrizevdev@gmail.com>
2025-07-12 21:46:53 +0300

    Update me.js

--- me.js
+++ me.js
@@ -1,21 +1,53 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
+const path = require('path');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Environment setup
+require('dotenv').config();
 const PORT = process.env.PORT || 8000;
-let code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
+// Security middleware
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+
+// Body parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:` + PORT)
-})
 
-module.exports = app
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+const pairRouter = require('./pair');
+app.use('/code', pairRouter);
+
+// HTML routes
+app.get('/pair', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pair.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Increase event listeners limit
+require('events').EventEmitter.defaultMaxListeners = 100;
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Pairing endpoint: http://localhost:${PORT}/pair`);
+});
+
+module.exports = app;
